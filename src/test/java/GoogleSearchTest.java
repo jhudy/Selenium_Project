0c3271aafcb4398d2bc026729a83360066ef5534
Judith Delgadillo <jhudy.delgadillo@gmail.com>
2019-12-11 22:29:58 -0400

    init project

---
+++ src/test/java/GoogleSearchTest.java
@@ -0,0 +1,38 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static junit.framework.TestCase.assertTrue;
+
+public class GoogleSearchTest {
+
+    private WebDriver driver;
+    By videoLinkLocator = By.cssSelector("a[href='https://www.youtube.com/watch?v=RbSlW8jZFe8']");
+
+    @Before
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver", "./src/test/resources/chromedriver/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.google.com/");
+    }
+
+    @Test
+    public void testGooglePage() throws InterruptedException {
+        WebElement searchBox = driver.findElement(By.name("q"));
+        searchBox.clear();
+        searchBox.sendKeys("QA Automation video");
+        searchBox.submit();
+        Thread.sleep(3000);
+        assertTrue(driver.findElement(videoLinkLocator).isDisplayed());
+    }
+
+    @After
+    public void tearDown(){
+        driver.quit();
+    }
+}
